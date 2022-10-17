c59a2e0cf657e028b29ed246260cabf2939df575
cynthia <cchelangat9892gmail.com>
2022-10-17 15:30:01 +0300

    commit tree 7b9221e0544e1d3111b035680fe53db5d05a8c81

--- methods.rb
+++ methods.rb
@@ -1,1 +1,12 @@
 # Your code here!
+def greet_programmer
+    print "Hello, programmer!"
+end
+
+def greet name
+    print "Hello, #{name}!"
+end
+
+# def greet_with_default(name= "programmer")
+    # print "Hello, #{name}!"
+
